ad6945bb442d19b42f002377b4ece3392670b033
Mary Crivelli <mary.crivelli@ibm.com>
2020-04-14 17:05:32 -0400

    removed unused tag

--- src/components/ArticleComponent.js
+++ src/components/ArticleComponent.js
@@ -11,7 +11,8 @@ class ArticleComponent extends React.Component {
     render(){
         return (
             <div className="article-component">
-                <div style=""><h1>{this.props.title}</h1><h2>{this.props.username}</h2></div>
+                <div><h1>{this.props.title}</h1>
+                <h2>{this.props.username}</h2></div>
                 <p>{this.props.body}</p>
                 {this.props.currentUser.userName !== this.props.username ? null :<button onClick={this.handleDelete}>delete</button>}
             </div>
